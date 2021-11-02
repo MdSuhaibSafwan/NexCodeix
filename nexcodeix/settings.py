32f843891e29392f7a4a1f7d8cef225a2bb9b72b
MD Suhaib Safwan <suhaibsafwan45@gmail.com>
2021-11-02 22:44:01 +0600

    added heroku supportive to project

--- nexcodeix/settings.py
+++ nexcodeix/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-nuc1v(8w&9ke6ekkyhqy_@a8(mzeeyy73r_)tow_al%ebt9u2w
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", ]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "nexcodeix.herokuapp.com"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
