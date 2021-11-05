e14af17411f05ae2b17d706f1aee74b31a51787d
MD Suhaib Safwan <68965547+MdSuhaibSafwan@users.noreply.github.com>
2021-11-05 12:46:43 +0600

    created the crud operations for batches

--- batch/models.py
+++ batch/models.py
@@ -39,6 +39,9 @@ class Batch(models.Model):
 
     date_created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ["-date_created", ]
+
     def __str__(self):
         return self.name
 
--- batch/views.py
+++ batch/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.contrib.auth import get_user_model
 from .models import Batch, BatchUser
 from .forms import BatchForm, BatchUserForm
@@ -6,4 +6,41 @@ from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned, 
 from django.views.generic import ListView, CreateView, UpdateView, DetailView
 
 
+class BatchListView(ListView):
+    template_name = "batch/staff/batch-list.html"
 
+    def get_queryset(self):
+        qs = Batch.objects.all()
+        return qs
+
+
+class BatchCreateView(CreateView):
+    template_name = "batch/staff/batch-create.html"
+    form_class = BatchForm
+
+    def form_valid(self, form):
+        """
+        If form's instance needs to be explicitely set
+        """
+        return super().form_valid(form)
+
+
+class BatchUpdateView(UpdateView):
+    template_name = "batch/staff/batch-create.html"
+    form_class = BatchForm
+    lookup_url_kwarg = "id"
+
+    def get_object(self):
+        batch_id = self.kwargs.get(self.lookup_url_kwarg)
+        obj = get_object_or_404(Batch, id=batch_id)
+        return obj
+
+    def form_valid(self, form):
+        """
+        If form's instance needs to be explicitely set
+        """
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
