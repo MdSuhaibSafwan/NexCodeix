449cd67aadcbf1aec59c9222f405007512621b64
MD Suhaib Safwan <68965547+MdSuhaibSafwan@users.noreply.github.com>
2021-11-25 18:57:32 -0800

    updated the task for class reminder

--- batches/admin.py
+++ batches/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Batch, BatchClass, ClassMaterials
+from .models import Batch, BatchClass, ClassMaterials, BatchUser
 
 admin.site.register(Batch)
 admin.site.register(BatchClass)
 admin.site.register(ClassMaterials)
+admin.site.register(BatchUser)
--- batches/tasks.py
+++ batches/tasks.py
@@ -1,6 +1,8 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 from .models import BatchClass, ClassMaterials
+from django.core.mail import send_mail
+from django.conf import settings
 
 
 @shared_task
@@ -13,5 +15,17 @@ def create_batch_class_task(**kwargs):
 def send_mail_for_class(batch_class):
     print("Inside Task ", batch_class)
     class_material = ClassMaterials.objects.create(batch_class=batch_class)
-    return class_material
+    batch = batch_class.batch
+    emails = batch.user.all().values_list("email")
+    
+    recipient_list = list(emails)
+
+    send_mail(
+        subject="Reminder of Class",
+        message="It is to remind you that class will be held exactly after 1 day",
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=recipient_list,
+    )
+
+    print("SENT MAILS FOR REMINDER OF BATCH CLASS TO ", recipient_list)
 
