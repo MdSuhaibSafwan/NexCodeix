143191c50efc9cd471e534df02df1b01f0545569
MdSuhaibSafwan <suhaibsafwan45@gmail.com>
2024-01-17 16:46:05 +0600

    made from info links to nex codeix

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+	path("", views.IndexPage.as_view(), name='index-page'),
+	path("project-detail/", views.ProjectDetailPage.as_view(), name="project-detail-page"),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,2 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class IndexPage(TemplateView):
+	template_name = "blog/index.html"
+
+
+
+class ProjectDetailPage(TemplateView):
+	template_name = "blog/project-detail.html"
 
